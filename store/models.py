71b4bce4aae7471ec5a84a23489b7a9d6a8385b7
unknown <sivabalaji464@gmail.com>
2021-05-26 18:04:29 +0530

    firstcommit

---
+++ store/models.py
@@ -0,0 +1,59 @@
+from django.db import models
+
+from users.models import User
+
+
+class Supplier(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, unique=True)
+    address = models.CharField(max_length=220)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Buyer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=120, unique=True)
+    address = models.CharField(max_length=220)
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    sortno = models.PositiveIntegerField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    STATUS_CHOICE = (
+        ('pending', 'Pending'),
+        ('decline', 'Decline'),
+        ('approved', 'Approved'),
+        ('processing', 'Processing'),
+        ('complete', 'Complete'),
+        ('bulk', 'Bulk'),
+    )
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    #quantity = models.PositiveIntegerField()
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICE)
+    created_date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.product.name
+
+
